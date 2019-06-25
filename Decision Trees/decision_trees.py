7e470443bef3702ac7d5a5c72e24e5302c49badf
Eytan Ohana <eohana98@gmail.com>
2019-06-26 02:08:28 +0300

    implemented calc_gini

---
+++ Decision Trees/decision_trees.py
@@ -0,0 +1,25 @@
+import numpy as np
+np.random.seed(42)
+
+chi_table = {0.01  : 6.635,
+             0.005 : 7.879,
+             0.001 : 10.828,
+             0.0005 : 12.116,
+             0.0001 : 15.140,
+             0.00001: 19.511}
+
+def calc_gini(data):
+    """
+    Calculate gini impurity measure of a dataset.
+
+    Input:
+    - data: any dataset where the last column holds the labels.
+
+    Returns the gini impurity of the dataset.
+    """
+    size = len(data)
+    classes = data[:,-1]
+    _, class_counts = np.unique(classes, return_counts=True)
+    gini = 1 - np.sum((class_counts / size) ** 2)
+
+    return gini
