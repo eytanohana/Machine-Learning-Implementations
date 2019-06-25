71eb0ae258364ae4d280f98e4a8b7c8ee163cc81
Eytan Ohana <eohana98@gmail.com>
2019-06-26 00:36:33 +0300

    Started notebook

---
+++ Linear Regression/linear_regression.py
@@ -0,0 +1,26 @@
+import numpy as np
+import itertools
+np.random.seed(42)
+
+def preprocess(X, y):
+    """
+    Perform mean normalization on the features and the labels
+ 
+    Input:
+    - X: Inputs  (n features over m instances).
+    - y: True labels.
+
+    Returns a two vales:
+    - X: The mean normalized inputs.
+    - y: The scaled labels.
+    """
+    
+    ###########################################################################
+    # TODO: Implement the normalization function.                             #
+    ###########################################################################
+    X = (X - X.mean(axis=0)) / (X.max(axis=0) - X.min(axis=0))
+    y = (y - y.mean()) / (y.max() - y.min())
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return X, y
