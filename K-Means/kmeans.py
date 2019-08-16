0f99814182606b5daf662fbeee114fdbc509cbe0
Eytan Ohana <eohana98@gmail.com>
2019-08-16 03:47:38 +0300

    Added docstrings to file.

--- K-Means/kmeans.py
+++ K-Means/kmeans.py
@@ -2,11 +2,27 @@ import numpy as np
 from skimage import io
 
 def get_random_centroids(X, k):
+    """
+    Randomly pick k centroids from the image.
+
+    :param X: An image of shape (num_pixels, 3)
+    :param k: The number of centroids to pick.
+    :return: Thr k centroids randomly chosen from the image.
+    """
     centroids = X[np.random.randint(len(X), size=k)]
     return centroids
 
 
 def lp_distance(X, centroids, p=2):
+    """
+    Computes the distances from every pixel in the image to each centroid
+    Using the Minkowski distance metric. Outputs an array of shape (k, num_pixels)
+
+    :param X: An image of shape (num_pixels, 3)
+    :param centroids: The centroids chosen from the image.
+    :param p: The distance metric to use.
+    :return: The distances from every pixel to the centroids.
+    """
     distances = np.abs(X.astype(float) - centroids[:, np.newaxis, :]) ** p
     distances = distances.sum(axis=2)
     distances = distances ** (1 / p)
@@ -16,6 +32,10 @@ def lp_distance(X, centroids, p=2):
 
 def kmeans(X, k, p, max_iter=100):
     """
+    Runs the k means algorithm until there's no improvement
+    in the assignments or until the maximum number of iterations
+    have passed.
+
     Inputs:
     - X: a single image of shape (num_features, 3).
     - k: number of centroids (i.e. colors).
@@ -45,6 +65,17 @@ def kmeans(X, k, p, max_iter=100):
     return centroids, classes
 
 def display_image(centroids, classes, img_shape):
+    """
+    Displays the image given the calculated centroids and classes of every pixel.
+
+    Inputs:
+    - centroids: The centroids of the image.
+    - classes: The class of each pixel.
+    - img_shape: The shape of the original image.
+
+    Output:
+    Displays the newly defined image.
+    """
     classes = classes.reshape(img_shape)
     compressed_image = np.zeros((classes.shape[0],classes.shape[1],3),dtype=np.uint8 )
     for i in range(classes.shape[0]):
