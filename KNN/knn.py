b5ea16f5cf5142e990f412e849d8ce93e501ad66
Eytan Ohana <eohana98@gmail.com>
2020-09-05 03:44:01 +0300

    implement simple knn

--- KNN/knn.py
+++ KNN/knn.py
@@ -2,3 +2,19 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 
+class KNNClassifier:
+    
+    def __init__(self, n_neighbors=5, p=2):
+        self.n_neighbors = n_neighbors
+        self.p = p
+        
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+        
+    def predict(self, X_test):
+        dists = (np.abs(self.X_train - X_test[:,np.newaxis,:]) ** self.p).sum(axis=2)
+        sorted_indices = dists.argsort()
+        closest_k = self.y_train[sorted_indices][:,:self.n_neighbors]
+        preds = stats.mode(closest_k, axis=1).mode
+        return preds
