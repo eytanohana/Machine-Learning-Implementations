27f2656da380343d8ef3de3ecd7ce2a35ed24b8d
eytanohana <eohana98@gmail.com>
2020-09-24 16:15:15 +0300

    add KNN reg class

--- KNN/knn.py
+++ KNN/knn.py
@@ -18,3 +18,20 @@ class KNNClassifier:
         closest_k = self.y_train[sorted_indices][:,:self.n_neighbors]
         preds = stats.mode(closest_k, axis=1).mode
         return preds
+    
+class KNNRegressor:
+    
+    def __init__(self, n_neighbors=5, p=2):
+        self.n_neighbors = n_neighbors
+        self.p = p
+        
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+    
+    def predict(self, X_test):
+        dists = (np.abs(self.X_train - X_test[:,np.newaxis,:]) ** self.p).sum(axis=2)
+        sorted_indices = dists.argsort()
+        closest_k = self.y_train[sorted_indices][:,:self.n_neighbors]
+        preds = closest_k.mean(axis=1)
+        return preds
