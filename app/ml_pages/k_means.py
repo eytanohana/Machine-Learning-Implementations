f79426ed37e8954fd5b58e8ab03987f029bfbfc8
eytan-ohana <eytan.ohana@traxretail.com>
2023-02-11 23:51:51 +0200

    start kmeans

--- app/ml_pages/k_means.py
+++ app/ml_pages/k_means.py
@@ -2,4 +2,12 @@ import streamlit as st
 
 
 def run():
-    st.header('K Means', anchor='k_means')
+    st.header('K-Means', anchor='k_means')
+    st.markdown('''
+    K-Means is an algorithm for grouping similar data into K predefined groups. In this app we'll use
+    K-Means to compress an image using color quantization, the process of compressing an image by representing
+    it using less colors.
+    ''')
+    image = st.file_uploader('Choose an image', accept_multiple_files=False)
+    if image:
+        st.image(image)
