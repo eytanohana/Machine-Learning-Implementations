cb0038da51c81d91d72d63b8a0f628f27a9902d7
eytan-ohana <eytan.ohana@traxretail.com>
2023-02-12 00:48:27 +0200

    expander;

--- app/ml_pages/k_means.py
+++ app/ml_pages/k_means.py
@@ -14,39 +14,39 @@ def run():
         st.stop()
     st.image(image)
     image = io.imread(image)
-    st.write(f'''
-    The shape of the image is: {image.shape}
-    
-    The first dimension, {image.shape[0]}, represents the height of the image, while the second, {image.shape[1]},
-    represents the width of the image, both being in pixels. The third dimension, {image.shape[2]}, represents the
-    different color channels of the image. Most traditionally, rgb, representing the red/green/blue intensities of
-    each channel for each pixel. The intensities ranging from 0 - 255.
-    ''')
+    with st.expander('Explanation'):
+        st.write(f'''
+        The shape of the image is: {image.shape}
+        
+        The first dimension, {image.shape[0]}, represents the height of the image, while the second, {image.shape[1]},
+        represents the width of the image, both being in pixels. The third dimension, {image.shape[2]}, represents the
+        different color channels of the image. Most traditionally, rgb, representing the red/green/blue intensities of
+        each channel for each pixel. The intensities ranging from 0 - 255.
+        ''')
 
-    image = image.reshape(image.shape[0] * image.shape[1], image.shape[2])
-    st.write(f'''
-    For the K-means algorithm, we need to reshape the data into two dimensions. The number of rows corresponding to
-    the number of pixels in the image and the number of columns representing the different color 
-    channels: {image.shape} - {len(image):,} pixels
-    ''')
+        image = image.reshape(image.shape[0] * image.shape[1], image.shape[2])
+        st.write(f'''
+        For the K-means algorithm, we need to reshape the data into two dimensions. The number of rows corresponding to
+        the number of pixels in the image and the number of columns representing the different color 
+        channels: {image.shape} - {len(image):,} pixels
+        ''')
 
-    st.markdown(r'''
-    ## The Algorithm
-    1. We start by choosing k random points, called the `centroids`.
-    1. We then assign every point in the dataset to the nearest centroid.
-        * All points belonging to the same centroid belong to the same "group".
-    1. We then calculate the mean point for each group and assign the means as the new centroids. 
-    1. We then repeat steps 2 and 3 until we don't see a change in the means or 
-    we reach a predetermined maximum number of iterations.
-    
-    ### The distance metric
-    To calculate the distance between two points, we use the Minkowski distance metric.
-    The Minkowski distance of order $p$ between two points: 
-    $\vec{x}=(x_1, ..., x_n)$ and $\vec{y}=(y_1, ..., y_n)$ is:
-    $$
-    D(\vec{x},\vec{y}) = (\sum_{i=1}^n \mid x_i - y_i \mid ^p)^{\frac{1}{p}}
-    $$
-    
-    The Minkowski distance is a generalization of the Euclidean ($p=2$) and Manhattan ($p=1$) distances.
-    ''')
+        st.markdown(r'''
+        ## The Algorithm
+        1. We start by choosing k random points, called the `centroids`.
+        1. We then assign every point in the dataset to the nearest centroid.
+            * All points belonging to the same centroid belong to the same "group".
+        1. We then calculate the mean point for each group and assign the means as the new centroids. 
+        1. We then repeat steps 2 and 3 until we don't see a change in the means or 
+        we reach a predetermined maximum number of iterations.
+        
+        ### The distance metric
+        To calculate the distance between two points, we use the Minkowski distance metric.
+        The Minkowski distance of order $p$ between two points: 
+        $\vec{x}=(x_1, ..., x_n)$ and $\vec{y}=(y_1, ..., y_n)$ is:
+        $$
+        D(\vec{x},\vec{y}) = (\sum_{i=1}^n \mid x_i - y_i \mid ^p)^{\frac{1}{p}}
+        $$
+        The Minkowski distance is a generalization of the Euclidean ($p=2$) and Manhattan ($p=1$) distances.
+        ''')
 
