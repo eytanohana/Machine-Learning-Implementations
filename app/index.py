8279782fb513e98fc32b319b6f1dff4b7ce25d8e
eytanohana <eohana98@gmail.com>
2023-09-10 00:52:20 +0300

    log version

--- app/index.py
+++ app/index.py
@@ -1,7 +1,10 @@
 import streamlit as st
+import os
 from ml_pages import k_means
 
 st.set_page_config('ML Deep Dive')
+logger = st.logger.get_logger(__name__)
+logger.info(f'App running on version {os.environ.get("DOCKER_TAG")}')
 
 pages = {
     'K Means': k_means
@@ -11,3 +14,4 @@ with st.sidebar:
     page = st.selectbox("Algorithms", pages.keys())
 
 pages[page].run()
+logger.info(f'Running page {page}')
